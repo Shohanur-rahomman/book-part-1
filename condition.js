c522573f8c4b32b44d58e3f57485872ce2cddf1a
Shohanur-rahomman <mdshohanur757@gmail.com>
2025-03-29 16:29:53 +0600

    first commit

---
+++ condition.js
@@ -0,0 +1,21 @@
+const prices = 3000;
+if (prices >= 6000) {
+    const discount = prices / 100 * 15;
+    const pay = prices - discount;
+    console.log(pay);
+} else if (prices >= 3000) {
+    const discount = prices / 100 * 5;
+    const pay = prices - discount;
+    console.log(pay);
+} else {
+    console.log(prices);
+}
+
+const age = 61 ;
+if (age <= 12) {
+     console.log('free');
+} else if (age > 60) {
+    console.log('50% discount');
+} else {
+    console.log('full payment');
+ }
---
+++ index.js
@@ -0,0 +1,40 @@
+const num1 = '56.34';
+const num2 = '47.25';
+
+const number = parseFloat(num1)
+const number2 = parseFloat(num2)
+const result = number + number2;
+// console.log(result);
+
+
+const price = 15000;
+if (price > 5000) {
+    const discount = price  * 0.1;
+    console.log(price - discount);
+
+} else {
+    console.log(price); 
+}
+
+
+const appelPrice = 160 ;
+if (appelPrice > 150) {
+    const discountPrice = appelPrice / 100 * 10;
+    const pay = appelPrice - discountPrice;
+    console.log(pay);
+} else {
+    console.log(appelPrice);
+}
+
+const mangoPrice = 5000;
+if (mangoPrice >= 5000) {
+    const discountPrice = mangoPrice / 100 * 20;
+    const pay = mangoPrice - discountPrice;
+    console.log(pay);
+} else if (mangoPrice >= 2500) {
+    const discountPrice = mangoPrice / 100 * 10;
+    const pay = mangoPrice - discountPrice;
+    console.log(pay);
+} else {
+    console.log(mangoPrice);
+}
